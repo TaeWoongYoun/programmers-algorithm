df0e437977613decca82db30f8717dc03d0784cb
윤태웅 <24457545yong@gmail.com>
2025-01-07 16:39:27 +0900

    [level 1] Title: 실패율, Time: 1922.52 ms, Memory: 15 MB -BaekjoonHub

--- Python3/프로그래머스/1/42889. 실패율/실패율.py
+++ Python3/프로그래머스/1/42889. 실패율/실패율.py
@@ -1,24 +1,12 @@
 def solution(N, stages):
-    answer = []
-    clear = len(stages)
-    prev_count = 0
-    index = []
-    result = []
-    for i in range(1, N+1):
-        count = 0
-        for j in stages:
-            if i == j:
-                count += 1
-        clear = clear - prev_count
-        prev_count = count
-        if count != 0:
-            answer.append(count / clear)
-        else:
-            answer.append(0)        
-    for i in range(len(answer)):
-        index.append([answer[i], i+1])
-    index.sort(key=lambda x: x[1])
-    index.sort(key=lambda x: x[0], reverse=True)  
-    for i, j in index:
-        result.append(j)
-    return result
+    rates = []  # [실패율, 스테이지] 저장할 리스트
+    players = len(stages)  # 전체 플레이어 
+    for stage in range(1, N+1):
+        if players:  # 플레이어가 있을 때만 실패율 계산
+            count = stages.count(stage)  # 현재 스테이지에 있는 플레이어 수
+            rates.append([count/players, stage])
+            players -= count
+        else:  # 플레이어가 없으면 실패율 0
+            rates.append([0, stage])
+    rates.sort(key=lambda x: (-x[0], x[1]))  # 실패율 내림차순, 스테이지 오름차순
+    return [stage for _, stage in rates]  # 스테이지 번호만 반환
