45cc4a1dd6761e88ab2c56adc700357fdcbcaeef
윤태웅 <24457545yong@gmail.com>
2025-01-17 19:41:11 +0900

    삼각형의 완성조건 （1）.c 업데이트

--- C/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.c
+++ C/프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.c
@@ -6,20 +6,17 @@ int solution(int sides[], size_t sides_len) {
     int answer = 0;
     
     for(int i=0;i<3;i++){
-        if(sides[i]>sides[i+1])
-        {
+        if(sides[i]>sides[i+1]){
             int temp=sides[i];
             sides[i]=sides[i+1];
             sides[i+1]=temp;
         }
     }
     
-    if(sides[0]+sides[1]>sides[2])
-    {
+    if(sides[0]+sides[1]>sides[2]){
         answer=1;
     }
-    else
-    {
+    else{
         answer=2;
     }
     return answer;
