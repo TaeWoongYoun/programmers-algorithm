ddeaf4f7440fc29506d61dfbf0c6f5bc059a4188
윤태웅 <24457545yong@gmail.com>
2025-02-18 15:59:22 +0900

    [level 0] Title: OX퀴즈, Time: 0.13 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -2,5 +2,5 @@ def solution(quiz):
     answer = []
     for i in quiz:
         left, right = i.split("=")
-        answer.append("O") if eval(left.strip()) == eval(right.strip()) else answer.append("X")
+        answer.append("O") if eval(left) == eval(right) else answer.append("X")
     return answer
