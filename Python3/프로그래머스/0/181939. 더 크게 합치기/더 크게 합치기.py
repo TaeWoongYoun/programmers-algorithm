37240fbd6806c8d76a6b822818e2aa798ba39eca
윤태웅 <24457545yong@gmail.com>
2025-02-24 21:14:00 +0900

    더 크게 합치기.py 업데이트

--- Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,8 +1,4 @@
 def solution(a, b):
     ab = int(str(a) + str(b))
     ba = int(str(b) + str(a))
-    
-    if ab >= ba :
-        return ab
-    else :
-        return ba
+    return ab if ab >= ba else ba
