c29c1aecd56fa0e3ec13673ad9f1a58caf77f9ca
윤태웅 <24457545yong@gmail.com>
2025-02-26 22:10:20 +0900

    원하는 문자열 찾기.py 업데이트

--- Python3/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ Python3/프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,7 +1,4 @@
 def solution(myString, pat):
     pat = pat.lower()
     myString = myString.lower()
-    if pat in myString:
-        return 1
-    else:
-        return 0
+    return 1 if pat in myString else 0
