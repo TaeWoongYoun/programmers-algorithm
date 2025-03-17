ab7ca433819c63208b0da75f18014a49955a7f7f
윤태웅 <24457545yong@gmail.com>
2025-03-17 21:53:37 +0900

    최댓값과 최솟값.py 업데이트

--- Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -3,10 +3,5 @@ def solution(s):
     
     minimum = min(numbers)
     maximum = max(numbers)
-    
-    # if minimum > maximum:
-    #     answer = f"{maximum} {minimum}"
-    # else:
-    #     answer = 
 
     return f"{minimum} {maximum}"
