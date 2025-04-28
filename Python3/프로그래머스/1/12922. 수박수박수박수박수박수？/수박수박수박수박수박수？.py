62cb500a0e1863bcde82bec52001440398dba7b3
윤태웅 <24457545yong@gmail.com>
2025-04-28 16:22:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.28 ms, Memory: 9.22 MB -BaekjoonHub

--- Python3/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ Python3/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,8 +1,5 @@
 def solution(n):
     answer = ''
     for i in range(1, n+1):
-        if i % 2 == 0:
-            answer += "박"
-        else:
-            answer += "수"
+        answer += "박" if i % 2 == 0 else "수"
     return answer
