51034089d836550fed0aa0a803b58c1f302ee6b7
윤태웅 <24457545yong@gmail.com>
2025-05-06 21:52:27 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 9.12 MB -BaekjoonHub

--- Python3/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ Python3/프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -3,12 +3,12 @@ def solution(array):
     max_count = 0
     answer = 0
     
-    for num in array:
-        counts[num] = counts.get(num, 0) + 1
-        if counts[num] > max_count:
-            max_count = counts[num]
-            answer = num
-        elif counts[num] == max_count:
+    for i in array:
+        counts[i] = counts.get(i, 0) + 1
+        if counts[i] > max_count:
+            max_count = counts[i]
+            answer = i
+        elif counts[i] == max_count:
             answer = -1
             
     return answer
