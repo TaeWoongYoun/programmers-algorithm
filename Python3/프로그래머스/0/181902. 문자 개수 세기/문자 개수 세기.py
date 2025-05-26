6343a89ff5cc2450a2b07aea6bf205d628f51e0b
윤태웅 <24457545yong@gmail.com>
2025-05-26 11:39:26 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

--- Python3/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ Python3/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,13 +1,7 @@
 def solution(my_string):
     answer = []
-    for i in range(65, 91):
-        if chr(i) in my_string:
-            answer.append(my_string.count(chr(i)))
-        else:
-            answer.append(0)
-    for j in range(97, 123):
-        if chr(j) in my_string:
-            answer.append(my_string.count(chr(j)))
-        else:
-            answer.append(0)
+    for i in range(65, 91): 
+        answer.append(my_string.count(chr(i))) if chr(i) in my_string else answer.append(0)
+    for j in range(97, 123): 
+        answer.append(my_string.count(chr(j))) if chr(j) in my_string else answer.append(0)
     return answer
