137c70342a870569d5d7276b140c3043e314fdf9
윤태웅 <24457545yong@gmail.com>
2025-05-26 16:32:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- Python3/프로그래머스/1/42748. K번째수/K번째수.py
+++ Python3/프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,8 @@
 def solution(array, commands):
-    answer, a, b, c, li = [], [], [], [], []
+    a, b, c, li = [], [], [], []
     
     for i in commands:
-        a.append(i[0])
-        b.append(i[1])
-        c.append(i[2])
+        a.append(i[0]), b.append(i[1]), c.append(i[2])
     
     for j in range(0, len(a)):
         num = sorted(array[a[j]-1:b[j]])
