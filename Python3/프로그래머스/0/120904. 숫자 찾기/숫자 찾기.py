4e60cdd8d2abd9d644568ad88f3083c849969a21
윤태웅 <24457545yong@gmail.com>
2025-06-09 17:03:49 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- Python3/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ Python3/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,6 +1,2 @@
 def solution(num, k):
-    num = list(str(num))
-    for i in range(len(num)):
-        if num[i] == str(k):
-            return i + 1
-    return -1
+    return list(str(num)).index(str(k)) + 1 if str(k) in str(num) else -1
